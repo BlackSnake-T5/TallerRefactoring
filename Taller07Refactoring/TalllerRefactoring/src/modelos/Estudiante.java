7495de49929cf681ff1e604b26acae94c4d83365
evelynohelia <enmejia@espol.edu.ec>
2020-08-13 20:33:22 -0500

    Update Estudiante.java

--- Taller07Refactoring/TalllerRefactoring/src/modelos/Estudiante.java
+++ Taller07Refactoring/TalllerRefactoring/src/modelos/Estudiante.java
@@ -2,15 +2,10 @@ package modelos;
 
 import java.util.ArrayList;
 
-public class Estudiante{
+public class Estudiante extends Persona{
     //Informacion del estudiante
     public String matricula;
-    public String nombre;
-    public String apellido;
     public String facultad;
-    public int edad;
-    public String direccion;
-    public String telefono;
     public ArrayList<Paralelo> paralelos;
     
     //Getter y setter de Matricula
@@ -23,24 +18,6 @@ public class Estudiante{
         this.matricula = matricula;
     }
 
-    //Getter y setter del Nombre
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    
-    //Getter y setter del Apellido
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-    
     //Getter y setter de la Facultad
     public String getFacultad() {
         return facultad;
@@ -50,33 +27,7 @@ public class Estudiante{
         this.facultad = facultad;
     }
     
-    //Getter y setter de la edad
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
     
-    //Getter y setter de la direccion
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-    
-    //Getter y setter del telefono
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
     
 }
         

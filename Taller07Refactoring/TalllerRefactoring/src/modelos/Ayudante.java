8e728846178132ab784ceea9452de4f6416612ff
David Santistevan <dsantistevana99@gmail.com>
2020-08-13 21:50:54 -0500

    Solucionado conflicto entre clase Ayudante y Estudiante

--- Taller07Refactoring/TalllerRefactoring/src/modelos/Ayudante.java
+++ Taller07Refactoring/TalllerRefactoring/src/modelos/Ayudante.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 
 public class Ayudante extends Estudiante{
     protected Estudiante est;
-    public ArrayList<Paralelo> paralelos;
+    public ArrayList<Paralelo> paralelosA;
 
     Ayudante(Estudiante e){
     	est = e;
     }
         
     public void agregarParalelo(Paralelo p){
-        paralelos.add(p);
+        paralelosA.add(p);
     }
 
 
     public void MostrarParalelos(){
-        for(Paralelo par:paralelos){
+        for(Paralelo par:paralelosA){
             //Muestra la info general de cada paralelo
         }
     }
